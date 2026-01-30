133bf11876eafc45bb7e216e1124b502ea303917
Wataru <watarun0208@icloud.com>
2026-01-30 12:12:06 +0900

    change boundary conditions and so on

--- scripts/run_sim.sh
+++ scripts/run_sim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. 引数でJSONファイルを指定（指定がなければデフォルトの *.json を使用）
-JSON_FILE="${1:-run.json}"
+JSON_FILE="${1:-kigumi-incline.json}"
 
 # 2. 現在の日時を取得 (月日_時分 例: 0127_1705)
 DATETIME=$(date +%m%d_%H%M)
